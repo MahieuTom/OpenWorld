fdfa5a479a8562450c985dc412bda4cee9cccfb7
ralph <ralph@Ralph-Mint>
2014-06-11 13:16:56 +0200

    camera, bugfixes, verschillende visualisations

--- camera.cpp
+++ camera.cpp
@@ -4,108 +4,87 @@
  * Initialiseer Toestenbord & Muis.
  */
 Camera::Camera() {
-    camPos = Coordinate(0, 0.0, 0.0);
+    camPos = Coordinate(-5, 1, 5);
     lookAt = Coordinate(0.0, 0.0, 0.0);
     upAxis = Coordinate(0.0, 1.0, 0.0);
 
     deltaAngle = 0.0f;
     angle = 0.0f;
-    pace = 0.5;
+    pace = 0.2;
 
-    //glutKeyboardFunc(processNormalKeys);
-    //glutSpecialFunc(Camera::keyPress);
-    //glutMotionFunc(Camera::mouseMove);
+    xOrigin = -1;
 }
 
 //-------------------------------------------------------------
 
 void Camera::update() {
-    lookAt.x = cos(angle) * cos(angle);
-    lookAt.y = sin(angle);
-    lookAt.z = sin(angle) * cos(angle);
-
-    //m_strafe_lx = cos(angle - M_PI_2);
-    //m_strafe_lz = sin(angle - M_PI_2);
-
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     gluLookAt(camPos.x, camPos.y, camPos.z,
-              lookAt.x, lookAt.y, lookAt.z,
-              upAxis.x,upAxis.y,upAxis.z);
+            camPos.x + lookAt.x, camPos.y + lookAt.y, camPos.z + lookAt.z,
+            upAxis.x, upAxis.y, upAxis.z);
 }
 
-/*void Camera::keyPress(int key, int x, int y) {
-    // this->camNorm.y -= 0.5; --> Naar beneden kijken.
-    // --------------------------------------------------------
-    // this->camPos.z += 0.5;  --> Over X-as bewegen.
-    // this->camNorm.z += 0.5;
-    // --------------------------------------------------------
-    //
+//-------------------------------------------------------------
 
+void Camera::keyPress(int key, int x, int y) {
     float fraction = 0.1f;
 
     switch (key) {
-        case GLUT_KEY_LEFT :
+        case GLUT_KEY_LEFT:
             angle -= 0.01f;
             lookAt.x = sin(angle);
             lookAt.z = -cos(angle);
             break;
-        case GLUT_KEY_RIGHT :
+        case GLUT_KEY_RIGHT:
             angle += 0.01f;
             lookAt.x = sin(angle);
             lookAt.z = -cos(angle);
             break;
-        case GLUT_KEY_UP :
-            camPos.x += lookAt.x * fraction;
-            camPos.z += lookAt.z * fraction;
+        case GLUT_KEY_UP:
+            /*
+                        camPos.x += lookAt.x * pace;
+                        camPos.z += lookAt.z * pace;
+             */
+            camPos.x += pace * lookAt.x; // *0.1f;
+            camPos.z += pace * lookAt.z; // *0.1f;
             break;
-        case GLUT_KEY_DOWN :
-            camPos.x -= lookAt.x * fraction;
-            camPos.z -= lookAt.z * fraction;
+        case GLUT_KEY_DOWN:
+            /*
+                        camPos.x -= lookAt.x * pace;
+                        camPos.z -= lookAt.z * pace;
+             */
+            camPos.x -= pace * lookAt.x; // *0.1f;
+            camPos.z -= pace * lookAt.z; // *0.1f;
             break;
     }
-
-    x=0, y=0;
 }
 
 //-------------------------------------------------------------
 
 void Camera::mouseMove(int x, int y) {
-    
-    POINT cursorPos;
-    GetCursorPos(&cursorPos);
-
-    //TODO:
-
-    double dx, dy;
-    if ((cursorPos.x != lookAt.x) || (cursorPos.y != lookAt.y)) {
-        dx = cursorPos.x - lookAt.x - viewportWidth/2;
-        dy = cursorPos.y - lookAt.y - viewportHeight/2;
-
-        //lookAt.x += dx;
-        //lookAt.y += dy;
-        //lookAt.z;
-
-        lookAt.x =  cos(dy) * sin(dx);
-        lookAt.y = -sin(dy);
-        lookAt.z =  cos(dy) * sin(dx);
-
-        //lookAt.x += dx;
-        //lookAt.y += dx;
-        //lookAt.z += dy;
+    // Enkel verplaatsen als linker muisknop ingedrukt wordt.
+    if (xOrigin >= 0) {
+        deltaAngle = (x - xOrigin) * 0.001f;
+        // Verander de richting.
+        lookAt.x = sin(angle + deltaAngle);
+        lookAt.z = -cos(angle + deltaAngle);
     }
+}
 
-    //SetCursorPos(viewportWidth/2, viewportHeight/2);
-    //
-    
+//-------------------------------------------------------------
 
+void Camera::mouseButton(int button, int state, int x, int y) {
+    // Enkel als er links wordt geklikt.
+    if (button == GLUT_LEFT_BUTTON) {
+        // Als het niet ingedrukt is.
+        if (state == GLUT_UP) {
+            angle += deltaAngle;
+            deltaAngle = 0.0f;
+            xOrigin = -1;
+        } else { // Als het ingedrukt is.
+            xOrigin = x;
 
-    // update deltaAngle
-    deltaAngle = (x + 1) * 0.001f;
-
-    // update camera's direction
-    lookAt.x = sin(angle + deltaAngle);
-    lookAt.z = -cos(angle + deltaAngle);
-
-    x=0, y=0;
-}*/
+        }
+    }
+}
